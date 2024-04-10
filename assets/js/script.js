7be57c195ac1ad07a84dbc9daa2600ad0491fbdb
Allan0913 <allan03.fiap@gmail.com>
2024-04-10 08:29:24 -0300

    adicionando javascript

---
+++ assets/js/script.js
@@ -0,0 +1,69 @@
+'use strict';
+
+
+/**
+ * navbar toggle
+ */
+
+const header = document.querySelector("[data-header]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+
+navToggleBtn.addEventListener("click", function () {
+  header.classList.toggle("nav-active");
+  this.classList.toggle("active");
+});
+
+/**
+ * toggle the navbar when click any navbar link
+ */
+
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    header.classList.toggle("nav-active");
+    navToggleBtn.classList.toggle("active");
+  });
+}
+
+
+
+
+
+
+/**
+ * voltar para o topo
+ */
+
+const backTopBtn = document.querySelector("[data-back-to-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+
+
+
+
+
+
+
+/**
+ * Função opinião de mostrar alerta e limpar o formulário
+ */
+
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  event.preventDefault(); 
+
+  //alerta
+  alert('Mensagem enviada com sucesso!');
+
+  document.getElementById('contactForm').reset();
+});
